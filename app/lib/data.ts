010b20c47b3c1cfac991ffe092bb06411f4d7847
zilkipradita <mr.zilkipradita@gmail.com>
2024-07-06 17:18:16 +0700

    update #11

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,7 +17,7 @@ export async function fetchRevenue() {
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const data = await sql<Revenue>`SELECT * FROM revenue`;
+    const data = await sql<Revenue>`SELECT * FROM revenue order by revenue desc`;
 
     // console.log('Data fetch completed after 3 seconds.');
 
