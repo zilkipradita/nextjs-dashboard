05dfe62ba1591234325547ea34ee017abaf24ec5
zilkipradita <mr.zilkipradita@gmail.com>
2024-07-12 14:41:07 +0700

    update products

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -61,7 +61,7 @@ export default async function Page() {
                 </thead>
                 <tbody className="bg-white">
 
-                {data?.map((datas) => (
+                {data?.map((datas : {id:string, name:string}) => (
 
                     <tr  key={datas.id} className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg">
                       <td className="whitespace-nowrap px-3 py-3">
